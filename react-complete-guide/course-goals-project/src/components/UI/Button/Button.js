0415b488e326929e69b976a6303fa62ebd650af1
Or Hasson <hassonor@gmail.com>
2022-03-20 18:03:23 +0200

    adding css module and styled components examples

--- react-complete-guide/course-goals-project/src/components/UI/Button/Button.js
+++ react-complete-guide/course-goals-project/src/components/UI/Button/Button.js
@@ -1,13 +1,42 @@
 import React from 'react';
-
-import './Button.css';
+import styles from './Button.module.css';
 
 const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+    return (
+        <button type={props.type} className={styles.button} onClick={props.onClick}>
+            {props.children}
+        </button>
+    )
+}
 
 export default Button;
+
+
+// Option B -> With Styled Components
+// import styled from "styled-components";
+//
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+//
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+//
+//   &:focus {
+//     outline: none;
+//   }
+//
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
