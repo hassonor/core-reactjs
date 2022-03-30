dee90c554183223a43d227ba3e5871dddf1b69f0
Or Hasson <hassonor@gmail.com>
2022-03-30 16:58:59 +0300

    updating add-user-model-project to react 18

--- react-complete-guide/add-user-model-project/src/index.js
+++ react-complete-guide/add-user-model-project/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 import './index.css';
 import App from './App';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>);
