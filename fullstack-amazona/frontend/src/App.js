7d55f1aca9cfe2a64afb7f3d573693edb3c8e9bf
Or Hasson <or@veloapp.io>
2022-05-10 19:25:11 +0300

    init amazona fe

---
+++ fullstack-amazona/frontend/src/App.js
@@ -0,0 +1,34 @@
+import data from "./data";
+
+function App() {
+    return (
+        <div>
+            <header>
+                <a href="/">amazona</a>
+            </header>
+            <main>
+                <h1> Featured Products</h1>
+                <div className="products">
+                    {
+                        data.products.map(product => (
+                            <div className="product" key={product.slug}>
+                                <a href={`/product/${product.slug}`}>
+                                    <img src={product.image} alt={product.name}/>
+                                </a>
+                                <div className="product-info">
+                                    <a href={`/product/${product.slug}`}>
+                                        <p>{product.name}</p>
+                                    </a>
+                                    <p><strong>{product.price}</strong></p>
+                                    <button>Add to cart</button>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default App;
