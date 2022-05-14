643c5562f5d11997ba762a3354f25f19c5c23e56
Or Hasson <or@veloapp.io>
2022-05-14 17:24:14 +0300

    adding generate Users with seed

--- fullstack-amazona/backend/data.js
+++ fullstack-amazona/backend/data.js
@@ -1,10 +1,27 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Basir',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true,
+        },
+        {
+            name: 'John',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false,
+        },
+    ],
     products: [
         {
+            // _id: '1',
             name: 'Nike Slim shirt',
             slug: 'nike-slim-shirt',
             category: 'Shirts',
-            image: '/images/p1.jpg',
+            image: '/images/p1.jpg', // 679px × 829px
             price: 120,
             countInStock: 10,
             brand: 'Nike',
@@ -13,18 +30,20 @@ const data = {
             description: 'high quality shirt',
         },
         {
+            // _id: '2',
             name: 'Adidas Fit Shirt',
             slug: 'adidas-fit-shirt',
             category: 'Shirts',
             image: '/images/p2.jpg',
             price: 250,
-            countInStock: 20,
+            countInStock: 0,
             brand: 'Adidas',
             rating: 4.0,
             numReviews: 10,
             description: 'high quality product',
         },
         {
+            // _id: '3',
             name: 'Nike Slim Pant',
             slug: 'nike-slim-pant',
             category: 'Pants',
@@ -37,6 +56,7 @@ const data = {
             description: 'high quality product',
         },
         {
+            // _id: '4',
             name: 'Adidas Fit Pant',
             slug: 'adidas-fit-pant',
             category: 'Pants',
