e7cc4d5f98ca3b1cc4ff5bb395cd371bbbbbab89
Or Hasson <or@veloapp.io>
2022-05-16 19:31:55 +0300

    test

---
+++ fullstack-amazona/frontend/src/screens/OrderScreen.js
@@ -0,0 +1,36 @@
+import React, {useEffect, useReducer} from 'react';
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return {...state, loading: true, error: ''};
+        case 'FETCH_SUCCESS':
+            return {...state, loading: false, order: action.payload, error: ''};
+        case 'FETCH_FAIL':
+            return {...state, loading: false, error: action.payload};
+        default:
+            return state;
+    }
+}
+
+export default function OrderScreen() {
+    const [{loading, error, order}, dispatch] = useReducer(reducer, {
+        loading: true,
+        order: {},
+        error: ''
+    });
+
+    useEffect(() => {
+
+    }, [])
+
+    return loading ? (
+        <LoadingBox></LoadingBox>
+    ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+    ) : (
+        <div></div>
+    )
+}
