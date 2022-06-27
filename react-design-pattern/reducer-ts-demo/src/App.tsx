d467180281287c7d9688a0da3ec2db33fc531626
Or Hasson <hassonor@gmail.com>
2022-06-27 22:18:04 +0200

    simple useReducer demo

---
+++ react-design-pattern/reducer-ts-demo/src/App.tsx
@@ -0,0 +1,10 @@
+import Notes from './Notes';
+
+
+function App() {
+    return (
+        <Notes/>
+    )
+}
+
+export default App;
---
+++ react-design-pattern/reducer-ts-demo/src/Notes.tsx
@@ -0,0 +1,108 @@
+import {useReducer, useState, ChangeEvent} from 'react'
+
+type TNote = {
+    id: number
+    note: string
+}
+
+type TAction = {
+    type: string
+    payload?: any
+}
+
+type TActionTypes = {
+    ADD: 'ADD'
+    UPDATE: 'UPDATE'
+    DELETE: 'DELETE'
+}
+
+const actionType: TActionTypes = {
+    ADD: 'ADD',
+    DELETE: 'DELETE',
+    UPDATE: 'UPDATE'
+}
+
+const initialNotes: TNote[] = [
+    {
+        id: 1,
+        note: 'Note 1'
+    },
+    {
+        id: 2,
+        note: 'Note 2'
+    }
+]
+
+const reducer = (state: TNote[], action: TAction) => {
+    switch (action.type) {
+        case actionType.ADD:
+            return [...state, action.payload]
+
+        case actionType.DELETE:
+            return state.filter(note => note.id !== action.payload)
+
+        case actionType.UPDATE:
+            const updatedNote = action.payload
+            return state.map((n: TNote) => n.id === updatedNote.id ? updatedNote : n)
+
+        default:
+            return state
+    }
+}
+
+const Notes = () => {
+    const [notes, dispatch] = useReducer(reducer, initialNotes)
+    const [note, setNote] = useState('')
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+
+        const newNote = {
+            id: Date.now(),
+            note
+        }
+
+        dispatch({type: actionType.ADD, payload: newNote})
+    }
+
+    return (
+        <div>
+            <h2>Notes</h2>
+
+            <ul>
+                {notes.map((n: TNote) => (
+                    <li key={n.id}>
+                        {n.note} {' '}
+                        <button
+                            onClick={() => dispatch({
+                                type: actionType.DELETE,
+                                payload: n.id
+                            })}
+                        >
+                            X
+                        </button>
+
+                        <button
+                            onClick={() => dispatch({
+                                type: actionType.UPDATE,
+                                payload: {...n, note}
+                            })}
+                        >
+                            Update
+                        </button>
+                    </li>
+                ))}
+            </ul>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder="New note"
+                    value={note}
+                    onChange={e => setNote(e.target.value)}
+                />
+            </form>
+        </div>
+    )
+}
+
+export default Notes
