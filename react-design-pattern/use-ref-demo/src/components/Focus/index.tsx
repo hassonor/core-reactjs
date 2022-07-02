9e33c328ebfb0ff8d02a44b9d59b2a2088cf1eeb
Or Hasson <hassonor@gmail.com>
2022-07-02 16:35:27 +0200

    Adding simple useRef focues

---
+++ react-design-pattern/use-ref-demo/src/components/Focus/index.tsx
@@ -0,0 +1,23 @@
+import {useRef} from 'react';
+
+
+const Focus = () => {
+    const inputRef = useRef<any>(null);
+
+    const handleClick = () => {
+        inputRef.current.focus();
+    }
+
+    return (
+        <>
+            <input
+                type="text"
+                ref={inputRef}
+            />
+            <button onClick={handleClick}>Set Focus</button>
+        </>
+    )
+}
+
+
+export default Focus;
