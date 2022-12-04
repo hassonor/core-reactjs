945560449d33b000af417f37319e936aedf4f906
Or Hasson <hassonor@gmail.com>
2022-12-04 03:58:36 -0800

    updating packages + fit to react 18

--- calling-apis-with-react/frontend/src/index.js
+++ calling-apis-with-react/frontend/src/index.js
@@ -1,19 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
+
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import CartStore from "./CartStore";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+);
+
+root.render(
     <React.StrictMode>
         <Provider store={CartStore}>
             <BrowserRouter>
                 <App/>
             </BrowserRouter>
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
