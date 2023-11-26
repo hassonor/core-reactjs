7070cf8f2cd002325a8a1fb52526ad267a2b700e
Or Hasson <hassonor@gmail.com>
2023-11-26 20:34:16 +0200

    remove not relvant examples

--- react-performance-optimization/src/CornerButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function CornerButton({ corner, setPosition, position }) {
-    return (
-        <div onClick={() => setPosition(corner)} className={`arrow arrow-${corner} ${position === corner ? 'arrow-active' : ''}`} />
-    );
-}
